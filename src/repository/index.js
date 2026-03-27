5c9ad7ba715131836a78b2111b3aff64ecfb95aa
Manishcs27 <manishxyz333@gmail.com>
2026-03-27 15:16:13 +0530

    Added city-service

---
+++ src/repository/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    CityRepository: require('./city_repository'),
+    AirportRepository: require('./airport_repository'),
+    FlightRepository: require('./flight_repository'),
+    SearchRepository: require('./search_repository')
+};
