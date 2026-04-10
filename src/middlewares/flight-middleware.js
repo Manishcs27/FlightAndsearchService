70f7ed3a7727aa8e37320e0c7b7c67370719408d
Manishcs27 <153353639+Manishcs27@users.noreply.github.com>
2026-04-10 15:42:54 +0530

    Added error code enums

--- src/middlewares/flight-middleware.js
+++ src/middlewares/flight-middleware.js
@@ -1,11 +1,14 @@
+const {ClientErrorCodes} = require('../utils/error-codes');
+
 const validateFlights = (req, res, next) => {
     if(!req.body.flightNumber || !req.body.airplaneId ||
          !req.body.departureTime || !req.body.arrivalTime || !req.body.departureAirportId || !req.body.arrivalAirportId || !req.body.price) {
-        return res.status(400).json({
+        return res.status(ClientErrorCodes.BAD_REQUEST).json({
             data:{},
-            sucess: false,
+            success: false,
             message: "Validation error",
-             error: "Missing required flight information" });
+            error: "Missing required flight information"
+        });
     }
     next();
 }
---
+++ src/utils/error-codes.js
@@ -0,0 +1,29 @@
+const { NOT } = require("sequelize/lib/deferrable");
+
+const ClientErrorCodes = Object.freeze({
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    INTERNAL_SERVER_ERROR: 500
+
+});
+
+const ServerErrorCodes = Object.freeze({
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_IMPLEMENTED: 501,
+});
+
+const SuccessCodes = Object.freeze({
+    OK: 200,
+    CREATED: 201,
+    ACCEPTED: 202,
+    NO_CONTENT: 204
+});
+
+module.exports = {
+    ClientError,
+    ServerError,
+    SuccessCodes
+}
